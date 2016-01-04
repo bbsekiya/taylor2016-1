01d34637df0053eba84b399cbf727e4ddbe8b756
bbsekiya <brian210003@gmail.com>
2016-01-04 09:45:27 -0800

    modified container01.cpp via develop

--- source/container01.cpp
+++ source/container01.cpp
@@ -26,59 +26,30 @@ class Container01
          return *this;
       }
   
-  Container01(const Container01&& other ) : m_S(nullptr) {
+  Container01(const Container01&& other ) : m_S("") {
          std::cout << "--- move constructor ----\n";
          m_S = other.m_S;
+         m_S = "";
       }
+      
+   Container01& operator=(Container01&& other) {
+      std::cout << "move assignment operator ---\n";
+  
+      if (this != &other) {     
+         m_S = other.m_S;
+         m_S = "";
+      }
+      return *this;
+   }
+   
+   
    private:
       std::string m_S;
 };
 typedef boost::shared_ptr<Container01>Container01Ptr;
 typedef boost::shared_ptr<const Container01>ConstContainer01Ptr;
 
-#if 0
-MemoryBlock(MemoryBlock&& other)
-   : _data(nullptr)
-   , _length(0)
-{
-   std::cout << "In MemoryBlock(MemoryBlock&&). length = " 
-             << other._length << ". Moving resource." << std::endl;
 
-   // Copy the data pointer and its length from the 
-   // source object.
-   _data = other._data;
-   _length = other._length;
-
-   // Release the data pointer from the source object so that
-   // the destructor does not free the memory multiple times.
-   other._data = nullptr;
-   other._length = 0;
-}
-
-// Move assignment operator.
-MemoryBlock& operator=(MemoryBlock&& other)
-{
-   std::cout << "In operator=(MemoryBlock&&). length = " 
-             << other._length << "." << std::endl;
-
-   if (this != &other)
-   {
-      // Free the existing resource.
-      delete[] _data;
-
-      // Copy the data pointer and its length from the 
-      // source object.
-      _data = other._data;
-      _length = other._length;
-
-      // Release the data pointer from the source object so that
-      // the destructor does not free the memory multiple times.
-      other._data = nullptr;
-      other._length = 0;
-   }
-   return *this;
-}
-#endif
 
 ConstContainer01Ptr c()
 {
