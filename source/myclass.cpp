6d8126b312c5e75ccfc0fbe6d88f5d9147fe8cc0
bbsekiya <brian210003@gmail.com>
2016-01-05 13:51:31 -0800

    added myclass.cpp

---
+++ source/myclass.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+class MyClass
+{
+   public:
+      explicit MyClass(int n1, int n2) : m_N1(n1), m_N2(n2) {std::cout << "--- constructor ---\n";}
+      virtual ~MyClass() {}
+      
+      MyClass(const MyClass& other) {
+         std::cout << "--- copy constructor ----\n";
+         m_N1 = other.m_N1;
+         m_N2 = other.m_N2;
+      }
+      
+      MyClass& operator=(const MyClass& other) {
+         if (this != &other) {
+            std::cout << "--- copy assignment operator ---\n";
+            m_N1 = other.m_N1;
+            m_N2 = other.m_N2;
+         }
+         return *this;
+      }
+      
+      MyClass(MyClass&& other) : m_N1(0), m_N2(0) {
+         std::cout << "--- move constructor ---\n";
+         m_N1 = other.m_N1;
+         m_N2 = other.m_N2;
+         
+         other.m_N1 = 0;
+         other.m_N2 = 0;
+      }
+      
+      MyClass& operator=(MyClass&& other) {
+         std::cout << " --- move assignment opeator ----\n";
+         if (this != &other) {
+            m_N1 = other.m_N1;
+            m_N2 = other.m_N2;
+            
+            other.m_N1 = 0;
+            other.m_N2 = 0;
+         }
+         return *this;
+       }
+       
+   private:
+      int   m_N1;
+      int   m_N2;
+};
+
+MyClass  test1()
+{
+   MyClass  c(1,2);
+   return c;
+}
+
+int main()
+{
+   MyClass     m1(100,200);
+   MyClass     m2 = m1;
+   MyClass     m3(std::move(test1()));
+   
+   return 0;
+}
+
+
+   
+      
+     
